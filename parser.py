07b6e9c545df7011c4684b32cf13f4b46bdf0373
Lapshov Konstantin <kosta-lap@yandex.ru>
2024-06-05 13:23:53 +0300

    change_local and parse_pgn added

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
-
-
+import re
 class Parser:
     def __init__(self, filename: str, *, platform: str) -> None:
         self._sep: str
+        self._isPgn: False
         match platform:
             case 'chess':
                 self._sep = ' '
@@ -11,12 +11,51 @@ class Parser:
             case _:
                 raise ValueError(f'This type of input data is not provided: {platform}')
 
+        if filename[-4:] == ".pgn":
+            self._isPgn = True
+            self._sep = ' '
+
         with open(filename, 'r', encoding='utf-8') as fp:
             self.data = fp.read()
-    
+
+    def change_local(self):
+        table = {
+            "N": "К",
+            "B": "C",
+            "R": "Л",
+            "K": "Кр",
+            "Q": "Ф"
+        }
+
+        result = list(self.data)
+        for literal in result:
+            if literal in table:
+                result[result.index(literal)] = table[literal]
+
+        self.data = "".join(result)
+
+    def parse_pgn(self):
+        moves = []
+
+        lines = self.data.split('\n')
+        moves_re = re.compile(r'(\d+\.\s+\S+\s+\S+)')
+
+        for line in lines:
+            moves_match = moves_re.findall(line)
+            if moves_match:
+                for move in moves_match:
+                    moves.append(move.strip())
+
+        self.data = " ".join(moves)
+
     def parse(self) -> dict[list[str]]:
+        self.change_local()
+
+        if self._isPgn:
+            self.parse_pgn()
+
         return self.parse_data(self.data, sep=self._sep)
-    
+
     @staticmethod
     def parse_data(data: str, *, sep: str) -> dict[list[str]]:
         data = data.split(sep)
@@ -33,7 +72,7 @@ class Parser:
                     res['white'].append(v)
                 case 2:
                     res['black'].append(v)
-        
+
         return res
 
 # type: ignore
